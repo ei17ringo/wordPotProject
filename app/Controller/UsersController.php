a6003978a1a24550c78404c2f176d4557fb2f223
yukasan <murakamiyuka.330@gmail.com>
2015-09-10 13:31:50 +0800

    Userのモデル、コントローラー追加

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+class PostsController extends AppController {
+
+	public function index() {}
+
+	public function index() {}
+
+	public function index() {}
+
+	public function index() {}
+
+}
