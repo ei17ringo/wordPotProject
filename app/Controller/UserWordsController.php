f31fa1aae984195a24c10f5c297086d2e4e450ef
yukiharry <k.yuki7676@gmail.com>
2015-09-10 13:20:30 +0800

    モデル（ユーザーワード、ワード）コントローラー（ユーザーワーズ）のアップ

---
+++ app/Controller/UserWordsController.php
@@ -0,0 +1,19 @@
+<?php
+class UserWordsController extends AppController{
+	public function mypage(){
+
+	}
+
+	public function add(){
+
+	}
+
+	public function edit(){
+
+	}
+
+	public function view(){
+		
+	}
+}
+?>
---
+++ app/Model/UserWord.php
@@ -0,0 +1,5 @@
+<?php
+class UserWord extends AppModel{
+	
+}
+?>
