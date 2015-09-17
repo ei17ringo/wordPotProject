f5b7d22025f03f46831b15617f010ec7bbba3ad1
Takuji Yoshioka <tkj.no3@gmail.com>
2015-09-17 15:19:44 +0800

    user profile index editページ最新

--- app/Controller/UserProfilesController.php
+++ app/Controller/UserProfilesController.php
@@ -26,7 +26,21 @@ class UserProfilesController extends AppController{
 		$userprofile = $this->UserProfile->find('first', array('conditions'=>$conditions));
 		$this->set('userprofile', $userprofile);
 
-	}
+	
+        
+        if($this->request->is(array('post', 'put'))){
+        	$this->UserProfile->id=$this->request->data['id'];
+            if($this->UserProfile->save($this->request->data)){
+                $this->Session->setFlash(__('更新されました'));
+                return $this->redirect(array('action' => 'index', $user_id));
+            }
+        }
+ 
+        if(!$this->request->data){
+            $this->request->data = $userprofile;
+        }
+    }
+	
 
 }
 
